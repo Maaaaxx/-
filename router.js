9f1c11d2c2c30c330e1777f9451750db6a9cd1c4
Maaaaxx <ma021024@126.com>
2017-10-23 19:08:17 +0800

    Create router.js

---
+++ router.js
@@ -0,0 +1,16 @@
+function setRouter(app){ 
+ var router = app; 
+
+
+
+
+app.get('/jsonp',function(req,res){
+
+    var data = 'JSONP success'  
+    if(req.query.callback){
+        res.send(req.query.callback + '(' + JSON.stringify(data) + ')')
+    } else{
+        res.send(JSON.stringify(data))
+    }
+})}
+ module.exports.setRouter = setRouter

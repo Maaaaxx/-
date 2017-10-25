8f1be227ab052758d8a6dbd3d9ca3f09a9a82498
Maaaaxx <ma021024@126.com>
2017-10-25 22:08:07 +0800

    Update router.js

--- CORS/router.js
+++ CORS/router.js
@@ -1,6 +1,7 @@
 app.get('/CORS', function (req,res) {
 
         res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
 
         var data = 'CORS SUCCESS'
 
